38c3bec272f77bb4c744863739f5d54ae3875837
David Konsumer <konsumer@jetboystudio.com>
2015-02-19 00:39:23 -0800

    removed extra var and wath

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ Ractive.components.gauge = module.exports = Ractive.extend({
   template: template,
   data: {
     label: 'gauge',
-    value: 0,
-    rot: 0
-  },
-  onrender: function(){
-    this.observe('value', function(val) {
-      this.set('rot', val*1.215);
-    });
+    value: 0
   }
 });
